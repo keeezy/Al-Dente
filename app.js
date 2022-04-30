a3104d4c6a28778850cc557f648eb2a17e94f3d9
Liberato Garced <93692943+lgarced@users.noreply.github.com>
2022-04-30 07:57:39 -0700

    Trying to fetch data

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-let recipes = 
-apiKey: "placeholder"
-featchRecipes: function(recipes) {
-    fetch()
-}
+
+
+
+let recipes = {
+apiKey: "383ad0ac5839254aad627d80d6afdaeb",
+apiId: "2f4e2d93",
+
+fetchRecipes: function(recipe) {
+    fetch(
+        "https://api.edamam.com/api/recipes/v2q=" + 
+        recipe + 
+        "app_id=" + 
+        this.apiId + "api_key" +
+        this.apiKey
+    )
+},
+
+  search: function () {
+    this.fetchRecipes(document.querySelector(".search").value);
+  }, 
     
 };
+
+
+
+document.querySelector(".search button").addEventListener("click", function () {
+  recipes.search();
+});
